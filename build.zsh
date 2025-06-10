43c93e1fea85b454b861cf2555ffdde34a5350b0
inThe-FLesh <rgrayy0099@tutamail.com>
2025-06-10 01:06:47 +0100

    Initial Commit.

---
+++ build.zsh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+set -e
+trap 'echo "❌ Build failed at line $LINENO"; exit 1' ERR
+
+echo "🔍 Checking for presence of build directory"
+
+if [[ -d build ]]; then
+  echo "✅ Build directory exists. Continuing build..."
+else
+  mkdir build
+  echo "📁 Build directory created."
+fi
+
+cd build
+
+echo "⚙️ Starting ninja build with Clang"
+
+cmake -G Ninja \
+  -DCMAKE_C_COMPILER=clang \
+  -DCMAKE_CXX_COMPILER=clang++ \
+  ..
+
+echo "🐻 Wrapping build with Bear"
+bear -- ninja --status "📦 [%f/%t] %o\n"
+
+ln -sf compile_commands.json ../compile_commands.json
+
+echo "🎉 Build Complete ✅"
